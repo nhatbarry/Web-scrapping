8c5b427e2d7df57862c6dfdfa3b70b65261b728c
nhatbarry <nhatbarry@gmail.com>
2024-11-08 16:26:22 +0700

    done pj

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,18 +6,33 @@ url = 'https://wakatime.com/leaders/language/html?page=1&country_code=VN'
 response = requests.get(url)
 
 
+import csv
+from bs4 import BeautifulSoup
+
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, 'html.parser')
     table = soup.find('table', class_='table leaders')
     rows = table.find_all('tr')
+    data = []
+
     for row in rows[1:]:
-        cols = row.findAll('td')
-        rank = cols[0].text
-        name = cols[1].text
-        hours = cols[2].text
-        daily = cols[3].text
-        language = cols[4].text
-        print(f"Rank {rank}:{name}\
-              \nHours of coded: {hours}\
-              \nDaily Average: {daily}\
-              \nLanguage used: {language}\n\n")
+        cols = row.find_all('td')
+        rank = cols[0].text.strip()
+        name = cols[1].text.strip()
+        hours = cols[2].text.strip()
+        daily = cols[3].text.strip()
+        language = cols[4].text.strip()
+        
+        data.append({
+            'rank': rank,
+            'name': name,
+            'hours of coded': hours,
+            'daily average': daily,
+            'language used': language
+        })
+
+    with open('leaderboard.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=['rank', 'name', 'hours of coded', 'daily average', 'language used'])
+        writer.writeheader()
+        writer.writerows(data)
+        print('this shit is done')
